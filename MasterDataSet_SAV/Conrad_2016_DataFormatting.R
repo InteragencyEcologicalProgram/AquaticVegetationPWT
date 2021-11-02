5ab467fe99500f9bb57d1f05b63a5f5aa8f2d05f
Rasmussen <Nicholas.Rasmussen@water.ca.gov>
2021-11-02 09:54:30 -0700

    Modified approach to filtering data based on the sample type column, created map of sampling locations, and modified column headers

--- MasterDataSet_SAV/Conrad_2016_DataFormatting.R
+++ MasterDataSet_SAV/Conrad_2016_DataFormatting.R
@@ -4,21 +4,26 @@
 #Conrad et al 2016 study
 
 #Notes
-#ask Louise for how dry mass per m^2 was calculated
-#need unique observation IDs; have that for each rake but not each spp within rake
-#there are water quality data associated with some of these samples but aren't in this data set
-#dissolved oxygen (mg/L), pH, specific conductance (μS), temperature (°C), and turbidity (NTU)
-#no WQ data for October 2008
-#get these from Louise
-#are rake numbers within site same location across dates?
+#are rake numbers within site same location across dates? Yes
+#need to confirm this by looking closer at data
+#if true, then create a station column from ID column (ie, site_site#_rake)
+
 #dropped 284 samples with missing GPS coordinates; see if there is a logical way to fill in some of these
 
+#rake dimensions from Conrad et al 2016
+#handle was 4.8 m long, so that is pretty much max possible sampling depth
+#rake head was 35 cm long, so when spun on axis this is the circle diameter
+#area sampled is then 3.14 *(0.35m/2)^2 = 0.101 m^2
+
+#sample labeling isn't perfectly accurate so don't rely on it: yes, no_no rake, and no_no sav
+#drop cases of no_no rake but make sure not to drop those mislabeled as no_no rake
 
 # Packages
 library(tidyverse) #suite of data science tools
 library(readxl) #read excel files
 library(janitor) #clean up column names
 library(sf) #convert GPS coordinate units
+library(deltamapr) #Sam's package with delta base layers
 
 # Read in the data----------------------------------------------
 # Data set is on SharePoint site for the 
@@ -40,11 +45,8 @@ conrad <- read_excel(path=paste0(sharepoint_path,"./ConradEtAl_SAVRakeData_20081
 
 # Formatting data set--------------------
 
-#look at categories for sample column
-unique(conrad$sample)
-#"no-no SAV", "yes", "no-no rake"
-#drop "no-no rake" because these are spots too deep to sample 
-#quite possible no SAV in these spots but can't know for sure either way
+#date range
+#range(conrad$gps_date) #"2008-10-07 UTC" "2010-10-18 UTC"
 
 #look at comment column
 comments <- conrad %>% 
@@ -52,11 +54,26 @@ comments <- conrad %>%
 unique(conrad$comment)
 #looks like comments didn't get read in
 #the few comments in the data set note occasions in which there is no biomass data
-#these are obvious in the data set even without comments (NA instead of zero for mass)
-#But in (at least) three cases, there was no SAV but data were NA when should be zero 
-#SAC_1_061809_R7, STE_1_061809_R8, WOO_1_061410_R4
+#these are generally obvious in the data set even without comments (NA instead of zero for mass)
 
+#explore depth data
+hist(as.numeric(conrad$depth))
+range(as.numeric(conrad$depth),na.rm = T) #0.1 14.8
+#rake handle is only 4.8 m long so exclude any depth measurements over 4.8 m
+
+#start formatting
 cleaner <- conrad %>% 
+  #drop samples with no-no rake
+  #use spp richness column to make sure no samples with SAV got mislabeled as no-no rake
+  #the extra code recovers one mislabeled sample
+  filter(!(sample=="no-no rake" & (spp_rich==0 | is.na(spp_rich) )))%>% 
+  #drop cases where spp richness is NA because it indicates no presence/absence data for sample
+  #note that sample type "yes" and "no-no SAV" aren't perfectly accurate so ignore them
+  filter(!(is.na(spp_rich))) %>% 
+  #change depth column from logical to numeric
+  mutate(across(c("depth"), as.numeric)) %>% 
+  #drop any cases in which depth is above the rake handle length (max sampling depth)
+  filter(depth < 4.8) %>% 
   #dropped unneeded columns
   select(-c("rake_key"
             ,"site_id"
@@ -65,14 +82,8 @@ cleaner <- conrad %>%
             ,"feat_name"
             ,"comment"
             ,"spp_rich":"s_lineage"
-            )
-         ) %>% 
-  #change depth column from logical to numeric
-  mutate(across(c("depth"), as.numeric)) %>% 
-  #drop sites that were too deep to sample
-  filter(sample!="no-no rake") %>% 
-  #drop sites where sample = yes but all or nearly all SAV columns contain NAs (n=33)
-  filter(!(sample=="yes" & is.na(egde))) %>% 
+  )
+  ) %>% 
   glimpse()
 
 #convert wide to long
@@ -115,40 +126,24 @@ long <- list(pa,wet,dry) %>%
   reduce(left_join, by=c("rake_id","species")) %>% 
   glimpse()
 
-#for presence/absence, there are only two options: zero or one
-#because only sites that were sampled are included here
-#those where sampling was attempted but failed because of deep water were already dropped
-
-#see if there are any cases of no SAV detected but data logged as NA instead of zero
-nsav_pa<- long %>% 
-  filter(sample=="no-no SAV" & is.na(pa)) #16 rows 
-#all from two samples
-
-#similarly, look at NAs for fresh biomass
+#look at NAs for fresh biomass
 nsav_f<- long %>% 
   filter(sample=="no-no SAV" & is.na(biomass_fresh_g)) 
-#25 rows, so 9 additional NAs beyond the ones for those two samples
+#9 NAs; all from WOO_1_061410_R4
 
-#similarly, look at NAs for dry biomass
+#look at NAs for dry biomass
 nsav_d<- long %>% 
   filter(sample=="no-no SAV" & is.na(biomass_dry_g)) 
-#25 rows; same number as fresh biomass
-
-unique(nsav_f$rake_id)
-unique(nsav_d$rake_id)
-#same three samples 
-#SAC_1_061809_R7: needs NAs changed to zeros for pa, fresh, and dry
-#STE_1_061809_R8: needs NAs changed to zeros for pa, fresh, and dry
-#WOO_1_061410_R4: needs NAs changed to zeros for fresh and dry (pa is zeros already)
+#9 NAs; all from WOO_1_061410_R4
+#WOO_1_061410_R4: needs NAs changed to zeros for fresh and dry
 #this is done in code below
 
-#look at pa for all samples (not just those without SAV)
+#look at pa for all samples 
 sav_pa<- long %>% 
-  filter(is.na(pa)) #17
+  filter(is.na(pa)) 
 unique(sav_pa$rake_id)
-#two are same as above: "SAC_1_061809_R7" "STE_1_061809_R8"
-#one additional one: "WAR_1_060909_R4" 
-#for all three samples, these NAs for pa should be converted to zeros
+#"WAR_1_060909_R4" 
+#NA for pa should be converted to zero
 #done in code below
 
 #look at fresh biomass for all samples
@@ -157,7 +152,6 @@ sav_f<- long %>%
 unique(sav_f$rake_id) #24 samples
 #20 samples from WOO_1 and VIC_1 6/14/2010, all but one of which should be NAs for wet and dry mass
 #exception: WOO_1_061410_R4 is a no SAV sample so we know mass is zeros for all species
-#plus "SAC_1_061809_R7" "STE_1_061809_R8", which should be zeros
 #plus "DIS_2_101209_R7": one NA that should be a zero
 #plus "BIG_1_102009_R3": one NA that should be a zero 
 
@@ -167,18 +161,16 @@ sav_d<- long %>%
 unique(sav_d$rake_id) #22 samples
 #20 samples from WOO_1 and VIC_1 6/14/2010, all but one of which should be NAs for wet and dry mass
 #exception: WOO_1_061410_R4 is a no SAV sample so we know mass is zeros for all species
-#plus "SAC_1_061809_R7" "STE_1_061809_R8", which should be zeros
 
 #look at cases of missing GPS coordinates
 lost <- long %>% 
   filter(is.na(easting)|is.na(northing))
 lost_samp<-unique(lost$rake_id) #284 samples are missing coordinates
 lost_date<-unique(lost$gps_date) #all samples from 9 dates in Oct 2010 
-#this issue is notes in metadata for this data set
+#this issue is noted in original metadata for this data set
 
-#if samples are taken at the same rake numbers each date,
-#then maybe we can copy coordinates from other dates
-#if each sample location is unique, we should drop all these samples with missing coordinates
+#samples are generally taken at the same rake numbers each date,
+#so maybe we can copy coordinates from other dates
 
 #are there samples with missing dates? probably not
 miss_date <- long %>% 
@@ -197,15 +189,12 @@ replacements <- c(tr$replacement)
 names(replacements) <- c(tr$target)
 
 #make corrections based on data exploration above and do some more formatting/cleaning
-#NOTE: maybe keep some version of the "sample" column instead of using the "incidence" column
-#that might make it easier to denote samples with no SAV
-#TO DO: drop the geometry column
 long_cleaner <- long %>% 
-  #all three samples with NAs for pa were were determined to be zeros so convert from NA
+  #one sample with NA for pa was were determined to be zero so convert from NA
   replace_na(list(pa=0)) %>% 
-  #fresh and dry mass for these three samples should be zeros instead of NAs (no SAV present)
+  #fresh and dry mass for this sample should be zeros instead of NAs (no SAV present)
   mutate(across(.cols = c(biomass_fresh_g,biomass_dry_g),
-                .fns = ~case_when(rake_id == "SAC_1_061809_R7" | rake_id == "STE_1_061809_R8" | rake_id == "WOO_1_061410_R4" 
+                .fns = ~case_when(rake_id == "WOO_1_061410_R4" 
                                   ~ as.numeric(0),
                                   TRUE ~ as.numeric(.x)))) %>% 
   #in this sample, one species was NA for fresh biomass but should be zero
@@ -221,11 +210,12 @@ long_cleaner <- long %>%
   #drop the sample column which is now redundant with the presence/absence column data
   #also drop the species nickname column now that we have latin names column
   select(-c("sample","species")) %>% 
-  #drop rows with missing GPS coordinates; see if there is a logical way to fill in some of these
+  #drop rows with missing GPS coordinates
+  #see if there is a logical way to fill in some of these
   filter(!is.na(easting) & !is.na(northing)) %>% 
   #rename some columns 
   rename("id"="rake_id"
-         ,"depth_m" = "depth"
+         ,"water_depth_m" = "depth"
          ,"incidence"="pa"
          ,"species"="species1"
          ,"date"="gps_date") %>% 
@@ -234,45 +224,89 @@ long_cleaner <- long %>%
              ,"survey_method"=as.character("rake_thatch")
              ) %>% 
   #add density columns; divides mass by area sampled by rake (0.101 m^2)
-  #these don't match the ones from the original file so check with Louise
   mutate("density_fresh_g_m^2"=round((biomass_fresh_g/0.101),2)
          ,"density_dry_g_m^2"=round((biomass_dry_g/0.101),2)
          ) %>% 
   #change CRS of sample coordinates
-  #specify the suspected CRS of original coordinate: UTM zone 10N (EPSG = 26910)
+  #specify the CRS of original coordinate: UTM NAD 83 (Zone 10N) (EPSG = 26910)
   st_as_sf(coords = c("easting", "northing"), crs = 26910) %>%
   #then transform to WGS84
   st_transform(4236) %>% 
   #then convert from geometry to columns
   mutate(latitude_wgs84 = unlist(map(geometry,2)),
          longitude_wgs84 = unlist(map(geometry,1))) %>% 
+  #drop the geometry column
+  st_set_geometry(NULL) %>% 
   #reorder columns
   select("program"
-         ,"id"
+         #,"id"
          , "date"
          , "latitude_wgs84"
          , "longitude_wgs84"
          , "species"
          , "survey_method"
-         , "incidence"
+         #probably don't need "incidence" now
+         #this info should be captured by 0 or NA
+         #but double check this 
+         #, "incidence" 
          , "biomass_fresh_g"
          , "biomass_dry_g"
          ,"density_fresh_g_m^2"
          ,"density_dry_g_m^2"
-         , "depth_m"
-         )
+         , "water_depth_m"
+         ) %>% 
+  glimpse()
 
-#look at total number of unique rake IDs
-#the number of rows should be 9x that number (ie, 9 species)
-rake_ids <- unique(long_cleaner$rake_id)
-#3874 x 9 = 34866 (which is the number of rows in df)
+#map all sampling locations
 
-#explore depth data a bit
-hist(long_cleaner$depth_m)
-range(long_cleaner$depth_m,na.rm = T) #0.10, 5.02
+#look at WW_Delta base map CRS
+#st_crs(WW_Delta)
+#CRS = NAD83, which is different than our sample data points
+#EPSG: 4269
 
+#prep sample coordinates for mapping
+gps <- long_cleaner %>% 
+  #select needed columns
+  select("date" 
+         ,"latitude_wgs84"
+         ,"longitude_wgs84"
+  ) %>% 
+  #reduce to just unique combinations of date and location
+  distinct(date,longitude_wgs84,latitude_wgs84) %>% 
+  #converting coordinates back to sf object
+  st_as_sf(coords = c(x='longitude_wgs84',y='latitude_wgs84') 
+           ,crs = 4236
+           ,remove=F
+           ) %>%  #EPSG code for WGS84
+  #transform to NAD83 (basically same as WGS84 though)
+  st_transform(crs = 4269) 
+
+#plot bay-delta base layer with sample locations 
+(sav_map_all <- ggplot()+
+    #plot waterways base layer
+    geom_sf(data= WW_Delta, fill= "skyblue3", color= "black") +
+    #plot all sampling locations from all dates
+    geom_sf(data= gps, fill= "red", color= "black", shape= 22, size= 3.5) +
+    #zoom in on region of delta where samples were collected
+    coord_sf( 
+     xlim =c(min(gps$longitude_wgs84)-0.1,max(gps$longitude_wgs84)+0.1),
+     ylim =c(min(gps$latitude_wgs84)-0.1,max(gps$latitude_wgs84)+0.1) 
+     )+
+    theme_bw()+
+    ggtitle('Nearshore Electrofishing Study')
+)
+#compared with map in Fig. 1 of Conrad et al 2016
+#sampling locations look correctly placed
+
+# Define path on SharePoint site for output files
+sharepoint_path_write <- normalizePath(
+  file.path(
+    Sys.getenv("USERPROFILE"),
+    "California Department of Water Resources/DWR - DSRS Aquatic Weed Control Action - MasterDataSet_SAV/Clean&Formatted"
+  )
+) 
 #write the formatted dataset to sharepoint site
-#write_csv(long_cleaner,file = paste0(sharepoint_path,"/ConradEtAl2016_formatted.csv"))
+#write_csv(long_cleaner,file = paste0(sharepoint_path_write,"/NearshoreEfishing_2008-2010_formatted.csv"))
 
 
 
