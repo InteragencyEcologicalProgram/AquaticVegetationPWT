163335b7b95173457725d814e29ce547509000f2
iep-coder <37346685+iep-coder@users.noreply.github.com>
2022-10-04 17:26:12 -0700

    decided to use csv versions instead of xlsx versions of raw data files. replaced species numbers with six letter codes. started formatting taxonomy file

--- MasterDataSet_SAV/Scripts_DataFormatting/FRP_DataFormatting.R
+++ MasterDataSet_SAV/Scripts_DataFormatting/FRP_DataFormatting.R
@@ -17,28 +17,91 @@ library(lubridate) #formatting dates
 library(sf) #work with GPS coordinates
 library(deltamapr) #maps of the delta
 
-#read in data-----------
+#read in data from excel files-----------
+#decided not to use these because they are missing some important fields that allow
+#tables to relate
 
 #site level data for day of visit like weather and water quality
-frp_site <-read_excel("Data_Raw/FRP/tables_needed/FRP_SiteVisit_29apr2022.xlsx") %>% 
+#frp_site1 <-read_excel("Data_Raw/FRP/tables_needed/FRP_SiteVisit_29apr2022.xlsx") %>% 
+#  clean_names()
+#same number of rows as csv version but fewer columns
+
+#sample level data like date, location, water depth
+#frp_sample1 <-read_excel("Data_Raw/FRP/tables_needed/FRP_Sample_29apr2022.xlsx")%>% 
+#  clean_names()
+#much fewer rows and columns than csv version
+
+#data for species on rake samples
+#frp_veg1 <-read_excel("Data_Raw/FRP/tables_needed/FRP_Vegetation_29apr2022.xlsx")%>% 
+#  clean_names()
+#same number of rows and columns as csv version
+
+#crosswalks numeric species codes and latin names and higher level taxonomy
+#will only need the subset of these that are for SAV species
+#frp_code1 <-read_excel("Data_Raw/FRP/tables_needed/FRP_VegetationCode_29apr2022.xlsx")%>% 
+#  clean_names()
+#same number of rows and columns as csv version
+
+#read in data from csv files--------------------
+#original files that contain all columns
+
+#site level data for day of visit like weather and water quality
+frp_site <-read_csv("Data_Raw/FRP/tables_all/SiteVisit.csv") %>% 
   clean_names()
 
 #sample level data like date, location, water depth
-frp_sample <-read_excel("Data_Raw/FRP/tables_needed/FRP_Sample_29apr2022.xlsx")%>% 
+frp_sample <-read_csv("Data_Raw/FRP/tables_all/Sample.csv")%>% 
   clean_names()
 
 #data for species on rake samples
-frp_veg <-read_excel("Data_Raw/FRP/tables_needed/FRP_Vegetation_29apr2022.xlsx")%>% 
+frp_veg <-read_csv("Data_Raw/FRP/tables_all/Vegetation.csv")%>% 
   clean_names()
 
 #crosswalks numeric species codes and latin names and higher level taxonomy
 #will only need the subset of these that are for SAV species
-frp_code <-read_excel("Data_Raw/FRP/tables_needed/FRP_VegetationCode_29apr2022.xlsx")%>% 
+frp_code <-read_csv("Data_Raw/FRP/tables_all/VegetationCode.csv")%>% 
   clean_names()
+#NOTE: I manually added a column that indicates whether a species is SAV
 
+#format the species level data--------
 
+#explore visit_no column
+visits <-unique(frp_veg$vist_no)
+#n = 350, but lots of NAs too
+#this matches to site visit level data
 
+#explore wet and dry weight columns
+hist(frp_veg$wet_weight)
+hist(frp_veg$dry_weight)
 
+#join species abundance data with taxonomy 
+veg_format <- left_join(frp_veg ,frp_code) %>% 
+  #drop unneeded columns
+  select(
+    visit_no = vist_no
+    ,sample_id_key
+    ,vegetation_id
+    ,species_code = vegetation_code
+    ,area
+    ,wet_weight
+    ,dry_weight
+    ,health_code
+  ) %>% 
+  glimpse()
+
+#how many occurrences of each species? 
+
+#should check to see if area % within a sample add to 100
+
+#create taxonomy table of just the SAV species in the field data--------
+
+taxonomy_field <- veg_format %>% 
+  distinct(species_code)
+#44 of 67 species in original vegetation code file
+
+taxonomy <- left_join(taxonomy_field,frp_code, by=c("species_code"="vegetation_code"),keep=F)
+#definitely stuff in here that isn't SAV
+#are these all rake samples or is this all survey data
 
 
 
