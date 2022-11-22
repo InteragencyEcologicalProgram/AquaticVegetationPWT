b31af8f3b3b056c964d7d3f06b7f8ef1ffe27ecb
iep-coder <37346685+iep-coder@users.noreply.github.com>
2022-11-21 17:29:10 -0800

    updated file paths and did trial run on combining two data sets

--- MasterDataSet_SAV/Scripts_DataFormatting/MasterDataSetBuilder.R
+++ MasterDataSet_SAV/Scripts_DataFormatting/MasterDataSetBuilder.R
@@ -12,25 +12,19 @@
 #required packages
 library(tidyverse)
 
-# Define path on SharePoint site for data
-# I synced this folder to my OneDrive
-dir_path <- normalizePath(
-  file.path(
-    Sys.getenv("USERPROFILE"),
-    "California Department of Water Resources/DWR - DSRS Aquatic Weed Control Action - MasterDataSet_SAV/Clean&Formatted"
-  )
-)  
-
 #import and merge the example csv files
 #https://statisticsglobe.com/merge-csv-files-in-r
-data_all <- list.files(path = dir_path,     # Identify all csv files in folder
-                       pattern = "*.csv", full.names = TRUE) %>% 
-  lapply(read_csv) %>%                                            # Store all files in list
-  bind_rows                                                       # Combine data sets into one data set 
-
+data_all <- list.files(path = "./Data_Formatted/",     # Identify all csv files in folder
+                       pattern = "*flatfile.csv", full.names = TRUE) %>% 
+  # Store all files in list
+  lapply(read_csv) %>% 
+  # Combine data sets into one data set
+  bind_rows %>%          
+  glimpse()
 
 
 #reorder columns
+#NEED TO WORK ON THIS
 data_all_ord <- data_all %>% 
   select("program"
          ,"station"
